ae1b8ef24431f2c63615bc26f2d998794cc3f4a7
ritwik-ethinos <ritwik.singh@ethinos.com>
2023-01-31 15:00:51 +0530

    post test

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,8 @@ const express = require('express');
 const homeRoute = express.Router();
 const homeController = require('../controllers/home')
 
-homeRoute.get('/', homeController.getBlogs)
+// homeRoute.get('/', homeController.getBlogs)
+homeRoute.post('/', homeController.getBlogs)
 // homeRoute.post('/', homeController.uploadBanner)
 
 module.exports = homeRoute;
