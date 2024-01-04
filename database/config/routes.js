23fdd50332a2da79dbfb158323aae98c6de4a9a0
ritwiksingh-tmpl <ritwik.r@tunica.tech>
2024-01-04 10:38:09 +0530

    Few changes

--- database/config/routes.js
+++ database/config/routes.js
@@ -1,14 +1,13 @@
-const homeRoute = require('../../routes/home')
-const welcome = require('../../routes/welcome')
-const searchRoute = require('../../routes/search')
-const articleRoute = require('../../routes/article')
-const subscribeRoute = require('../../routes/subscribe')
+const homeRoute = require("../../routes/home");
+const welcome = require("../../routes/welcome");
+const searchRoute = require("../../routes/search");
+const articleRoute = require("../../routes/article");
+const subscribeRoute = require("../../routes/subscribe");
 
 module.exports = (app) => {
-    console.log("this is a test");
-    app.use('/', welcome);
-    app.use('/home', homeRoute);
-    app.use('/search', searchRoute);
-    app.use('/article', articleRoute);
-    app.use('/subscribe', subscribeRoute)
-}
+  app.use("/", welcome);
+  app.use("/home", homeRoute);
+  app.use("/search", searchRoute);
+  app.use("/article", articleRoute);
+  app.use("/subscribe", subscribeRoute);
+};
--- database/models/index.js
+++ database/models/index.js
@@ -1,39 +1,51 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname,'../config/config.js'))[env];
+const env = process.env.NODE_ENV || "development";
+const config = require(path.join(__dirname, "../config/config.js"))[env];
 const db = {};
 
-
 let sequelize;
-// cloud database
-let db_uri = process.env.DB_URI
+// if database URI
+let db_uri = process.env.DB_URI;
 
 if (db_uri) {
-sequelize = new Sequelize(db_uri)
-console.log("cloud database is running...")
-// local database
+  sequelize = new Sequelize(db_uri);
+  console.log(
+    "\n------------------- Connected to the Database using DB URI-------------------\n"
+  );
+  // if database Credentials
 } else {
-sequelize = new Sequelize(config.database, config.username, config.password, config);
-console.log("local database is running...")
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+  console.log(
+    "\n------------------- Connected to the Database Using Creds-------------------\n"
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
